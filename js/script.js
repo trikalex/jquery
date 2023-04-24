8287ba436462131d25b7a3ceb9d1a1d75b755bb1
Alexandru Ursu <bodhipankh@yahoo.com>
2023-04-24 01:52:23 +0000

    Add effect exercise code

--- js/script.js
+++ js/script.js
@@ -1,9 +1,6 @@
 $(document).ready(function() {
-    $("#stream1_btn").click(function() {
-        $(".stream1").removeClass('highlight_stream');
-        $(".stream2").removeClass('highlight_stream');
-        $(".stream3").removeClass('highlight_stream');
-        $(".stream1").addClass('highlight_stream');
+    $("#stream1_btn").on(function() {
+        $(".stream1").fadeTo(1000, 0.5);
    });
    $("#stream2_btn").on("click", function() {
         $(".stream1").removeClass('highlight_stream');
