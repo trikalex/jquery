355eb1953f648ca0fc31f3c30d538efa7d9dbd72
Alexandru Ursu <bodhipankh@yahoo.com>
2023-04-24 01:55:59 +0000

    Prepare files for the method chaining exercise

---
+++ 03-jQueryEvents/02-events_in_jquery/video_solution/script.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+    $("#stream1_btn").on(function() {
+        $(".stream1").fadeTo(1000, 0.5);
+   });
+   $("#stream2_btn").on("click", function() {
+        $(".stream1").removeClass('highlight_stream');
+        $(".stream2").removeClass('highlight_stream');
+        $(".stream3").removeClass('highlight_stream');
+        $(".stream2").addClass('highlight_stream');
+   });
+   $("#stream3_btn").on("click", function() {
+        $(".stream1").removeClass('highlight_stream');
+        $(".stream2").removeClass('highlight_stream');
+        $(".stream3").removeClass('highlight_stream');
+        $(".stream3").addClass('highlight_stream');
+   });
+}); 
