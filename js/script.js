c71aa8c1f1a2ed67453cbff46279269b3bf50490
Alexandru Ursu <bodhipankh@yahoo.com>
2023-04-26 00:51:37 +0000

    Prep correct files for exercise

--- js/script.js
+++ js/script.js
@@ -1,13 +1,10 @@
-$(".stream-nav").on("click", function() {
-    // A selector to match all cards in all streams
-    var allStreamsCardsSelector = ".card";
-    // A selector to match just this stream's cards
-    // for this, we use the class with the name of the stream,
-    // which is the same as this nav link's id and then the "-card" suffix.
-    var thisStreamCardsSelector = "." + this.id + "-card";
-
-    // First remove the highlight from all of the cards
-    $(allStreamsCardsSelector).removeClass("card-highlight");
-    // Then apply the highlight to just this stream's cards
-    $(thisStreamCardsSelector).addClass("card-highlight");
+$(document).ready(function() {
+    $(".box").on("click", function() {
+    	/**
+    	 * When we click on an element that has
+    	 * a 'box' class, this event will be fired.
+    	 */
+    	var classNames = $(this).attr("class").split(" ");
+        $("." + classNames[1]).css("background-color", "red");
+    });
 });
